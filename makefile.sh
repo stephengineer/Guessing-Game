fad7f68795673607112acb433ab64465dd47dcc0
Stephen Wang <zhongqi1112@gmail.com>
2018-10-01 14:30:08 -0700

    Delete makefile.sh

--- makefile.sh
+++
@@ -1,6 +0,0 @@
-Guessing-Game: README.md
-	echo "# Guessing Game" > README.md
-	echo "## Date: `date`" >> README.md
-	echo "## Number of lines: `wc -l < guessinggame.sh`" >> README.md
-clean :
-	rm README.md
