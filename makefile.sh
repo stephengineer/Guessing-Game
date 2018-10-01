7bc59b480fff33eca3e6b9a104b15ed562a7cc65
Stephen Wang <zhongqi1112@gmail.com>
2018-10-01 14:27:13 -0700

    Update makefile.sh

--- makefile.sh
+++ makefile.sh
@@ -1,4 +1,6 @@
-README.md:
+Guessing-Game: README.md
 	echo "# Guessing Game" > README.md
 	echo "## Date: `date`" >> README.md
 	echo "## Number of lines: `wc -l < guessinggame.sh`" >> README.md
+clean :
+	rm README.md
